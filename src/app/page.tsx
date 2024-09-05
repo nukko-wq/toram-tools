f2813b54b3f1de327ba7763c4516f25039b86a2b
nukko <shouta.tsuboi@gmail.com>
2024-09-06 08:30:05 +0900

    #35 デザインの変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,39 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="flex flex-col justify-center min-h-screen">
-      <div className="mt-10">
-      <h1 className="text-center text-3xl font-bold text-gray-700">
-        トーラムいろいろツール
-      </h1>
-      <div className="w-full text-center mt-8">
-        <ul>
-          <li>
-            <Link href="/market" className="text-lg text-blue-600 hover:underline">マーケット計算</Link>
-          </li>
-        </ul>
-      </div>
+      <div className="mt-10 flex flex-col justify-center items-center">
+        <div>
+          <h1 className="text-center text-3xl font-bold text-gray-700">
+            トーラムいろいろツール
+          </h1>
+        </div>
+        <div className="max-w-lg p-6 mt-8 border border-gray-200 rounded-lg shadow">
+          <Link
+            href="/market"
+          >
+            <h5 className="text-2xl font-bold text-gray-700">マーケット計算</h5>
+          </Link>
+          <p className="mb-3 font-normal text-gray-700">
+            トーラムオンラインのマーケット単価計算や税抜価格の計算が出来ます。
+            <br />
+            最安値で出品したい場合や国際マーケットで決まった値段で出品したいときにどうぞ。
+          </p>
+          <Link
+            href="/market"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-500 rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+          >
+            Learn more
+            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </Link>
+        </div>
       </div>
       <footer className="mt-auto items-center p-4">
         <aside className="items-center flex flex-row justify-center">
-          <p className="text-sm text-gray-500">© トーラムいろいろツール 2024</p>
+          <p className="text-sm text-gray-500">
+            © トーラムいろいろツール 2024
+          </p>
         </aside>
       </footer>
     </main>
