b94b5200c85bdddc3f4236579f53cafa18a52daa
nukko <shouta.tsuboi@gmail.com>
2024-09-11 11:39:47 +0900

    #50 Biomeを導入した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,63 +1,66 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 import Favicon from '/public/images/Metadata/favicon.ico'
 import AppleIcon from '/public/images/Metadata/apple-icon.png'
 import AndroidIcon from '/public/images/Metadata/android-touch-icon.png'
 import OgImage from '/public/images/Metadata/og-image.jpg'
 import TwitterImage from '/public/images/Metadata/twitter-image.jpg'
-import { Analytics } from "@vercel/analytics/react"
-import { GoogleAnalytics } from "@next/third-parties/google"
+import { Analytics } from '@vercel/analytics/react'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "トーラムいろいろツール",
-  description: "トーラムオンラインのいろいろツール。マーケット計算ツールがあります。",
-  robots: {
-    index: true,
-    follow: true,
-  },
-  openGraph: {
-    title: "トーラムいろいろツール",
-    description: "トーラムオンラインのいろいろツール。マーケット計算ツールがあります。",
-    type: "website",
-    images: [
-      {
-        url: OgImage.src,
-        width: 1200,
-        height: 630
-      }
-    ]
-  },
-  twitter: {
-    card: 'summary',
-    title: 'トーラムいろいろツール',
-    description: 'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
-    images: TwitterImage.src
-  },
-  verification: {
-    google: "6OpGn6zf_DC6Sq376ndFl1wJ47WnLtXf0YK4Q6qW8u4",
-  },
-  icons: [
-    { rel: 'icon', url: Favicon.src },
-    { rel: 'apple-touch-icon', url: AppleIcon.src, sizes: "180x180"},
-    { rel: 'icon', type: "image/png", url: AndroidIcon.src, sizes: "192x192"}
-  ]
+	title: 'トーラムいろいろツール',
+	description:
+		'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+	robots: {
+		index: true,
+		follow: true,
+	},
+	openGraph: {
+		title: 'トーラムいろいろツール',
+		description:
+			'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+		type: 'website',
+		images: [
+			{
+				url: OgImage.src,
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
+	twitter: {
+		card: 'summary',
+		title: 'トーラムいろいろツール',
+		description:
+			'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+		images: TwitterImage.src,
+	},
+	verification: {
+		google: '6OpGn6zf_DC6Sq376ndFl1wJ47WnLtXf0YK4Q6qW8u4',
+	},
+	icons: [
+		{ rel: 'icon', url: Favicon.src },
+		{ rel: 'apple-touch-icon', url: AppleIcon.src, sizes: '180x180' },
+		{ rel: 'icon', type: 'image/png', url: AndroidIcon.src, sizes: '192x192' },
+	],
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="ja">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
-      <GoogleAnalytics gaId="G-GZ32LPEYKN" />
-    </html>
-  )
+	return (
+		<html lang="ja">
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+			</body>
+			<GoogleAnalytics gaId="G-GZ32LPEYKN" />
+		</html>
+	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,51 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <main className="flex flex-col justify-center min-h-screen">
-      <div className="mt-10 flex flex-col justify-center items-center">
-        <div>
-          <h1 className="text-center text-3xl font-bold text-gray-700">
-            トーラムいろいろツール
-          </h1>
-        </div>
-        <div className="max-w-lg p-6 mt-8 mx-2 border border-gray-200 rounded-lg shadow">
-          <Link
-            href="/market"
-          >
-            <h5 className="text-2xl font-bold text-gray-700">マーケット計算</h5>
-          </Link>
-          <p className="mb-3 font-normal text-gray-700">
-            トーラムオンラインのマーケット最安価格の計算や税抜き価格の計算が出来ます。
-            <br />
-            最安値で出品したい場合や国際マーケットで決まった値段で出品したいときにどうぞ。
-          </p>
-          <Link
-            href="/market"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-500 rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 "
-          >
-            Learn more
-            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-          </Link>
-        </div>
-      </div>
-      <footer className="mt-auto items-center p-4">
-        <aside className="items-center flex flex-row justify-center">
-          <p className="text-sm text-gray-500">
-            © トーラムいろいろツール 2024
-          </p>
-        </aside>
-      </footer>
-    </main>
-  )
+	return (
+		<main className="flex flex-col justify-center min-h-screen">
+			<div className="mt-10 flex flex-col justify-center items-center">
+				<div>
+					<h1 className="text-center text-3xl font-bold text-gray-700">
+						トーラムいろいろツール
+					</h1>
+				</div>
+				<div className="max-w-lg p-6 mt-8 mx-2 border border-gray-200 rounded-lg shadow">
+					<Link href="/market">
+						<h5 className="text-2xl font-bold text-gray-700">マーケット計算</h5>
+					</Link>
+					<p className="mb-3 font-normal text-gray-700">
+						トーラムオンラインのマーケット最安価格の計算や税抜き価格の計算が出来ます。
+						<br />
+						最安値で出品したい場合や国際マーケットで決まった値段で出品したいときにどうぞ。
+					</p>
+					<Link
+						href="/market"
+						className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-500 rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+					>
+						Learn more
+						<svg
+							className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 10"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M1 5h12m0 0L9 1m4 4L9 9"
+							/>
+						</svg>
+					</Link>
+				</div>
+			</div>
+			<footer className="mt-auto items-center p-4">
+				<aside className="items-center flex flex-row justify-center">
+					<p className="text-sm text-gray-500">© トーラムいろいろツール 2024</p>
+				</aside>
+			</footer>
+		</main>
+	)
 }
