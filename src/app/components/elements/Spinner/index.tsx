76818df57b6a034d1fb4eb413829534f95b9fa1e
nukko <shouta.tsuboi@gmail.com>
2024-09-24 08:58:06 +0900

    ローディングのスタイルを変更

--- src/app/components/elements/Spinner/index.tsx
+++ src/app/components/elements/Spinner/index.tsx
@@ -1,8 +1,16 @@
 const Spinner = () => {
   return (
+    <>
+      {/*}
     <div className="flex w-full items-center justify-center">
       <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-black" />
     </div>
+    */}
+      <div className="flex justify-center items-center gap-6 mt-10" >
+        <div className="h-10 w-10 animate-spin border-[5px] border-sky-400 rounded-full  border-t-transparent" />
+        <p className="text-[30px] font-weight">Loading</p>
+      </div>
+    </>
   )
 }
 
