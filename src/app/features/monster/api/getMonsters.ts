17f10287885d6be893724c98cc20290854862d3e
nukko <shouta.tsuboi@gmail.com>
2024-09-23 20:59:40 +0900

    キャッシュの修正

--- src/app/features/monster/api/getMonsters.ts
+++ src/app/features/monster/api/getMonsters.ts
@@ -1,12 +1,13 @@
 import { client } from '@/libs/microcms'
 import type { Queries } from '../types'
 import type { Monsters } from '../types'
+import { cache } from 'react'
 
-const getMonsters = async ({ q }: Queries) => {
+const getMonsters = cache(async ({ q }: Queries) => {
 	return await client.getAllContents<Monsters>({
 		endpoint: 'monster',
 		queries: { q },
 	})
-}
+})
 
 export default getMonsters
