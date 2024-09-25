002b582125f162827cf668fee997f9b44eb87a61
nukko <shouta.tsuboi@gmail.com>
2024-09-25 20:39:30 +0900

    スタイリングの変更

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -14,9 +14,10 @@ const Page = () => {
   return (
     <>
       <Header title="マーケット計算" link="/market" />
-      <div className="flex flex-col min-h-screen bg-gray-200">
-        <div className="w-full md:mt-4 px-4 py-4 ">
-          <div className="md:mx-auto px-4 py-4 max-w-xl bg-white shadow-md rounded-lg">
+      <div className="flex flex-col bg-gray-200 flex-grow 2xl:justify-center">
+
+        <div className="w-full md:mt-4 2xl:mt-0 px-4 py-4 2xl:flex items-start 2xl:mx-auto justify-center">
+          <div className="mx-auto px-4 py-4 max-w-xl 2xl:w-full bg-white shadow-md rounded-lg 2xl:mr-4 2xl:ml-0">
             <h1 className="text-3xl font-bold text-gray-700">マーケット最安計算</h1>
             <div className="w-full max-w-xl items-center mt-5">
               <div className="">
@@ -25,7 +26,7 @@ const Page = () => {
             </div>
           </div>
 
-          <div className="md:mx-auto mt-4 px-4 py-4 max-w-xl bg-white shadow-md rounded-lg">
+          <div className="mx-auto mt-4 2xl:mt-0 px-4 py-4 max-w-xl 2xl:w-full bg-white shadow-md rounded-lg 2xl:mx-4">
             <h1 className="text-3xl font-bold text-gray-700">税抜き価格計算</h1>
             <div className="w-full max-w-xl mt-5">
               <div className="">
@@ -34,7 +35,7 @@ const Page = () => {
             </div>
           </div>
 
-          <div className="md:mx-auto mt-4 px-4 py-4 max-w-xl bg-white shadow-md rounded-lg">
+          <div className="mx-auto mt-4 2xl:mt-0 px-4 py-4 max-w-xl 2xl:w-full bg-white shadow-md rounded-lg 2xl:mr-0 2xl:ml-4">
             <h1 className="text-3xl font-bold text-gray-700">税込み価格計算</h1>
             <div className="w-full max-w-xl mt-5">
               <div className="">
