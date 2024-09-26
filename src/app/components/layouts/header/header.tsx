87d7143562f767fb5733130dd71836f390d43dea
nukko <shouta.tsuboi@gmail.com>
2024-09-26 22:16:20 +0900

    ナビゲーションメニューの作成

--- src/app/components/layouts/header/header.tsx
+++ src/app/components/layouts/header/header.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import { auth } from '@/auth'
 import MobileMenu from '@/src/app/components/elements/MobileMenu/MobileMenu'
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu'
+import { CaretDownIcon } from '@radix-ui/react-icons'
 
 export default async function Header({ title, link }: { title: string, link: string }) {
 	const session = await auth()
@@ -13,6 +15,34 @@ export default async function Header({ title, link }: { title: string, link: str
 				<Link href="/" scroll={false}>トーラムいろいろツール</Link>
 			</div>
 			<div className="hidden md:block md:absolute md:left-1/2 md:transform md:-translate-x-1/2 font-bold flex-grow text-center text-lg lg:text-xl opacity-90"><Link href={link}>{title}</Link></div>
-		</header>
+			{session && (
+				<NavigationMenu className='mr-4 hidden md:block'>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NavigationMenuTrigger>
+								ツール{' '}
+								<CaretDownIcon
+									className="relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+									aria-hidden
+								/>
+							</NavigationMenuTrigger>
+							<NavigationMenuContent>
+								<ul className="grid gap-3 p-6 md:w-[250px] lg:w-[250px] lg:grid-cols-1 relative -left-[160px] top-4 shadow rounded-lg bg-white">
+									<NavigationMenuLink href="/market" className={`hover:bg-teal-100 hover:text-teal-800 px-6 opacity-80 text-xl font-bold py-6 ${navigationMenuTriggerStyle()}`}>
+										マーケット計算
+									</NavigationMenuLink>
+									<NavigationMenuLink href="/monster" className={`hover:bg-blue-100 hover:text-blue-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
+										モンスター検索
+									</NavigationMenuLink>
+									<NavigationMenuLink href="/colors" className={`hover:bg-rose-100 hover:text-rose-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
+										染色一覧
+									</NavigationMenuLink>
+								</ul>
+							</NavigationMenuContent>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			)}
+		</header >
 	)
 }
