1d93e7847f9424796fdea6bb5118f36911fc089d
nukko <shouta.tsuboi@gmail.com>
2024-09-27 11:33:15 +0900

    アバターアイコンの追加

--- src/app/components/layouts/header/header.tsx
+++ src/app/components/layouts/header/header.tsx
@@ -3,6 +3,7 @@ import { auth } from '@/auth'
 import MobileMenu from '@/src/app/components/elements/MobileMenu/MobileMenu'
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu'
 import { CaretDownIcon } from '@radix-ui/react-icons'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 
 export default async function Header({ title, link }: { title: string, link: string }) {
 	const session = await auth()
@@ -11,37 +12,43 @@ export default async function Header({ title, link }: { title: string, link: str
 	return (
 		<header className="flex items-center justify-between border-b z-50 w-full h-16 fixed top-0 shadow-custom backdrop-blur-[5px]">
 			<MobileMenu isLoggedIn={isLoggedIn} />
-			<div className='font-bold text-base lg:text-xl absolute md:static md:ml-4 left-1/2 transform -translate-x-1/2 md:-translate-x-0 opacity-90 text-blue-500'>
+			<div className='flex-grow font-bold text-base lg:text-xl absolute md:static md:ml-4 left-1/2 transform -translate-x-1/2 md:-translate-x-0 opacity-90 text-blue-500'>
 				<Link href="/" scroll={false}>トーラムいろいろツール</Link>
 			</div>
 			<div className="hidden md:block md:absolute md:left-1/2 md:transform md:-translate-x-1/2 font-bold flex-grow text-center text-lg lg:text-xl opacity-90"><Link href={link}>{title}</Link></div>
 			{session && (
-				<NavigationMenu className='mr-4 hidden md:block'>
-					<NavigationMenuList>
-						<NavigationMenuItem>
-							<NavigationMenuTrigger>
-								ツール{' '}
-								<CaretDownIcon
-									className="relative top-[1px] transition-transform duration-250 ease-in group-data-[state=open]:-rotate-180"
-									aria-hidden
-								/>
-							</NavigationMenuTrigger>
-							<NavigationMenuContent>
-								<ul className="grid gap-3 p-6 md:w-[250px] lg:w-[250px] lg:grid-cols-1 relative -left-[160px] top-4 shadow-md rounded-lg bg-white">
-									<NavigationMenuLink href="/market" className={`hover:bg-teal-100 hover:text-teal-800 px-6 opacity-80 text-xl font-bold py-6 ${navigationMenuTriggerStyle()}`}>
-										マーケット計算
-									</NavigationMenuLink>
-									<NavigationMenuLink href="/monster" className={`hover:bg-blue-100 hover:text-blue-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
-										モンスター検索
-									</NavigationMenuLink>
-									<NavigationMenuLink href="/colors" className={`hover:bg-rose-100 hover:text-rose-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
-										染色一覧
-									</NavigationMenuLink>
-								</ul>
-							</NavigationMenuContent>
-						</NavigationMenuItem>
-					</NavigationMenuList>
-				</NavigationMenu>
+				<>
+					<NavigationMenu className='mr-4 hidden md:block'>
+						<NavigationMenuList>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>
+									ツール{' '}
+									<CaretDownIcon
+										className="relative top-[1px] transition-transform duration-250 ease-in group-data-[state=open]:-rotate-180"
+										aria-hidden
+									/>
+								</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<ul className="grid gap-3 p-6 md:w-[250px] lg:w-[250px] lg:grid-cols-1 relative -left-[160px] top-4 shadow-md rounded-lg bg-white">
+										<NavigationMenuLink href="/market" className={`hover:bg-teal-100 hover:text-teal-800 px-6 opacity-80 text-xl font-bold py-6 ${navigationMenuTriggerStyle()}`}>
+											マーケット計算
+										</NavigationMenuLink>
+										<NavigationMenuLink href="/monster" className={`hover:bg-blue-100 hover:text-blue-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
+											モンスター検索
+										</NavigationMenuLink>
+										<NavigationMenuLink href="/colors" className={`hover:bg-rose-100 hover:text-rose-800 font-bold px-6 opacity-80 text-xl py-6 ${navigationMenuTriggerStyle()}`}>
+											染色一覧
+										</NavigationMenuLink>
+									</ul>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+					<Avatar className='w-11 h-11 mr-4'>
+						<AvatarImage src={session.user?.image || ""} alt="ユーザーアバター" className='rounded-full' />
+						<AvatarFallback>{session.user?.name?.[0] || 'U'}</AvatarFallback>
+					</Avatar>
+				</>
 			)}
 		</header >
 	)
