e0b9a56380c5c1a7720a67cbccbe72431cba222d
nukko <shouta.tsuboi@gmail.com>
2024-12-24 07:59:36 +0900

    不要なインポートの削除

--- src/app/colors/layout.tsx
+++ src/app/colors/layout.tsx
@@ -1,64 +1,59 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import '@/src/app/globals.css'
 import Favicon from '/public/images/Metadata/favicon.ico'
 import AppleIcon from '/public/images/Metadata/monster/apple-icon.png'
 import AndroidIcon from '/public/images/Metadata/monster/android-touch-icon.png'
 import OgImage from '/public/images/Metadata/og-image.jpg'
 import TwitterImage from '/public/images/Metadata/twitter-image.jpg'
-import { Analytics } from '@vercel/analytics/react'
-import { GoogleAnalytics } from '@next/third-parties/google'
 import Header from '../components/layouts/header/header'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
-  title: 'トーラム色一覧 | トーラムいろいろツール',
-  description:
-    'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
-  robots: {
-    index: false,
-    follow: false,
-  },
-  openGraph: {
-    title: 'トーラムいろいろツール',
-    description:
-      'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
-    type: 'website',
-    images: [
-      {
-        url: OgImage.src,
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary',
-    title: 'トーラムいろいろツール',
-    description:
-      'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
-    images: TwitterImage.src,
-  },
-  verification: {
-    google: '6OpGn6zf_DC6Sq376ndFl1wJ47WnLtXf0YK4Q6qW8u4',
-  },
-  icons: [
-    { rel: 'icon', url: Favicon.src },
-    { rel: 'apple-touch-icon', url: AppleIcon.src, sizes: '180x180' },
-    { rel: 'icon', type: 'image/png', url: AndroidIcon.src, sizes: '192x192' },
-  ],
+	title: 'トーラム色一覧 | トーラムいろいろツール',
+	description:
+		'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+	robots: {
+		index: false,
+		follow: false,
+	},
+	openGraph: {
+		title: 'トーラムいろいろツール',
+		description:
+			'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+		type: 'website',
+		images: [
+			{
+				url: OgImage.src,
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
+	twitter: {
+		card: 'summary',
+		title: 'トーラムいろいろツール',
+		description:
+			'トーラムオンラインのいろいろツール。マーケット計算ツールがあります。',
+		images: TwitterImage.src,
+	},
+	verification: {
+		google: '6OpGn6zf_DC6Sq376ndFl1wJ47WnLtXf0YK4Q6qW8u4',
+	},
+	icons: [
+		{ rel: 'icon', url: Favicon.src },
+		{ rel: 'apple-touch-icon', url: AppleIcon.src, sizes: '180x180' },
+		{ rel: 'icon', type: 'image/png', url: AndroidIcon.src, sizes: '192x192' },
+	],
 }
 
 export default function MonsterLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode
 }>) {
-  return (
-    <>
-      <Header title="トーラム色一覧" link='/colors' />
-      {children}
-    </>
-  )
+	return (
+		<>
+			<Header title="トーラム色一覧" link="/colors" />
+			{children}
+		</>
+	)
 }
