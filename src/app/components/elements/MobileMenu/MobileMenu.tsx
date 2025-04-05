9a4431621ba78d2fcdd6c469f45c84440c34f1b8
nukko <shouta.tsuboi@gmail.com>
2025-04-06 08:21:17 +0900

    ログイン機能の削除

--- src/app/components/elements/MobileMenu/MobileMenu.tsx
+++ src/app/components/elements/MobileMenu/MobileMenu.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { SignOut } from '../SignOutButton/SignOutButton'
 
 const MobileMenu = () => {
 	const [isOpen, setIsOpen] = useState(false)
