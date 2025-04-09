a228671ee54c392557b6d2f99e1343e9b9443080
nukko <shouta.tsuboi@gmail.com>
2025-04-09 17:58:18 +0900

    imageのパスを変更

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,12 +6,12 @@ export default function manifest(): MetadataRoute.Manifest {
 		description: 'トーラムのいろいろなツール',
 		icons: [
 			{
-				src: '/images/Metadata/android-touch-icon.png',
+				src: '/android-touch-icon.png',
 				type: 'image/png',
 				sizes: '192x192',
 			},
 			{
-				src: '/images/Metadata/android-touch-icon-512.png',
+				src: '/android-touch-icon-512.png',
 				type: 'image/png',
 				sizes: '512x512',
 			},
