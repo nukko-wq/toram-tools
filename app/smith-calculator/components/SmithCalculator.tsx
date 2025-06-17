2769fb244df793974d2e4d501641c0412483a36e
nukko <shouta.tsuboi@gmail.com>
2025-06-17 21:00:04 +0900

    Styleの修正

--- app/smith-calculator/components/SmithCalculator.tsx
+++ app/smith-calculator/components/SmithCalculator.tsx
@@ -171,7 +171,7 @@ export default function SmithCalculator() {
 	}
 
 	return (
-		<div className="max-w-7xl mx-auto p-4 lg:p-6 pb-20 md:pb-6 space-y-6 lg:space-y-8">
+		<div className="max-w-7xl mx-auto p-4 lg:p-6 pb-20 md:pb-6 space-y-6 lg:space-y-8 lg:mt-6">
 			<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-[1fr_2fr] xl:gap-8 gap-4 md:gap-6 lg:gap-0 lg:space-y-6">
 				{/* 左カラム: キャラクター */}
 				<div className="gap-4 md:gap-6 lg:col-span-3 xl:col-span-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1">
