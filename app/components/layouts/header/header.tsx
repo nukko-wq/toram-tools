12161b9edeaaaf4e00bd9dcb7e1cd0a8616d618d
nukko <shouta.tsuboi@gmail.com>
2025-06-22 16:41:19 +0900

    outlineの色を調整

--- app/components/layouts/header/header.tsx
+++ app/components/layouts/header/header.tsx
@@ -12,12 +12,14 @@ export default async function Header({
 		<header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b shadow-custom backdrop-blur-[5px]">
 			<MobileMenu />
 			<div className="-translate-x-1/2 md:-translate-x-0 absolute left-1/2 grow transform font-bold text-base text-blue-500 md:static md:ml-4 lg:text-xl">
-				<Link href="/" scroll={false}>
+				<Link href="/" scroll={false} className="outline-blue-500">
 					トーラムいろいろツール
 				</Link>
 			</div>
 			<div className="md:-translate-x-1/2 hidden grow text-center font-bold text-lg text-slate-700 md:absolute md:left-1/2 md:block md:transform lg:text-xl">
-				<Link href={link}>{title}</Link>
+				<Link href={link} className="outline-blue-500">
+					{title}
+				</Link>
 			</div>
 		</header>
 	)
